f41d27a5e20c349aed247456b052df80575d76b3
TimothyAllman <166746823+TimothyAllman@users.noreply.github.com>
2024-08-19 22:53:00 +0200

    fix relative path issue

--- MyPipeline/tasks/ConstantsFromFilePaths.py
+++ MyPipeline/tasks/ConstantsFromFilePaths.py
@@ -46,10 +46,63 @@ import pickle
 # <br>
 # <br>
 # <br>
-# <h1> Folders we will create and use </h1>
+# <h1> Folders paths </h1>
+# We could create folders here and then use them
+#
+# ```
+# e.g. ABSOLUTE_PATH_TO_DATA_FOLDER = f"../MyDataFolder"
+# ```
+#
+# but this poses the problem that the `../` part is relative to whatever directory we are running the code from
+# and if we run it in different contexts (i.e. when debugging the notebook vs when running the entire piepline) the relative path is different
+# i.e. we get this error
+#
+# >
+# > FileNotFoundError: [Errno 2] No such file or directory: '../Data/olympic_athletes.csv'
+# >
+# in simple terms we could get errors because this is a relative path
+#
+# To fix we need to create the folder in the pipeline.yaml (similar to how we would create any other shared variable)
+# e.g.
+#
+# ```
+#  product:
+#      nb: products/{{MY_FOLDER}}/notebooks/ConstantsFromFilePaths.ipynb
+#
+#      # hardcoded/defined paths for top level folders
+#      HARD_CODED_PATH_TO_DATA_FOLDER: ../MyDataFolder
+# ```
+#
+# and then inside this task we bring it into scope using `str()`
+#
+# ```
+# ABSOLUTE_PATH_TO_DATA_FOLDER = str(product["HARD_CODED_PATH_TO_DATA_FOLDER"])
+# ```
+#
+# However this does have the unintended consequence of creating `.MyDataFolder.metadata` files
+# inside the same same folder that `MyDataFolder` exists in
+# so it might be good to check
+# TODO: check if just having ../ in pipeline yaml is better and then use tht and build the various folders in here
+# i.e. try
+#
+# ```
+#  product:
+#      nb: products/{{MY_FOLDER}}/notebooks/ConstantsFromFilePaths.ipynb
+#
+#      # hardcoded/defined paths for top level folders
+#      HARD_CODED_PATH_TO_TOP_LEVEL_FOLDER: ../
+# ```
+#
+# and then inside
+#
+# ```
+# ABSOLUTE_PATH_TO_TOP_LEVEL_FOLDER = str(product["HARD_CODED_PATH_TO_DATA_FOLDER"])
+# ABSOLUTE_PATH_TO_DATA_FOLDER = f"{ABSOLUTE_PATH_TO_TOP_LEVEL_FOLDER}\MyDataFolder"
+# ```
+#
 # %%
-ABSOLUTE_PATH_TO_DATA_FOLDER = f"../Data"
-
+ABSOLUTE_PATH_TO_DATA_FOLDER = str(product["HARD_CODED_PATH_TO_DATA_FOLDER"])
+ABSOLUTE_PATH_TO_DATA_FOLDER
 # %%
 
 # %%
@@ -80,33 +133,33 @@ ABSOLUTE_PATH_TO_DATA_FOLDER = f"../Data"
 # %%
 ABSOLUTE_PATH_TO_ATHLETES_CSV_FILE = f"{ABSOLUTE_PATH_TO_DATA_FOLDER}/olympic_athletes.csv"
 # pickle it to use it later
-Path(product["ABSOLUTE_PATH_TO_ATHLETES_CSV_FILE"]).parent.mkdir(exist_ok=True,parents=True)
+Path(product["ABSOLUTE_PATH_TO_ATHLETES_CSV_FILE"]).parent.mkdir(exist_ok=True, parents=True)
 Path(product["ABSOLUTE_PATH_TO_ATHLETES_CSV_FILE"]).write_bytes(pickle.dumps(ABSOLUTE_PATH_TO_ATHLETES_CSV_FILE))
-#print output
+# print output
 ABSOLUTE_PATH_TO_ATHLETES_CSV_FILE
 
 # %%
-ABSOLUTE_PATH_TO_HOSTS_CSV_FILE= f"{ABSOLUTE_PATH_TO_DATA_FOLDER}/olympic_hosts.csv"
+ABSOLUTE_PATH_TO_HOSTS_CSV_FILE = f"{ABSOLUTE_PATH_TO_DATA_FOLDER}/olympic_hosts.csv"
 # pickle it to use it later
-Path(product["ABSOLUTE_PATH_TO_HOSTS_CSV_FILE"]).parent.mkdir(exist_ok=True,parents=True)
+Path(product["ABSOLUTE_PATH_TO_HOSTS_CSV_FILE"]).parent.mkdir(exist_ok=True, parents=True)
 Path(product["ABSOLUTE_PATH_TO_HOSTS_CSV_FILE"]).write_bytes(pickle.dumps(ABSOLUTE_PATH_TO_HOSTS_CSV_FILE))
-#print output
+# print output
 ABSOLUTE_PATH_TO_HOSTS_CSV_FILE
 
 # %%
 ABSOLUTE_PATH_TO_MEDALS_CSV_FILE = f"{ABSOLUTE_PATH_TO_DATA_FOLDER}/olympic_medals.csv"
 # pickle it to use it later
-Path(product["ABSOLUTE_PATH_TO_MEDALS_CSV_FILE"]).parent.mkdir(exist_ok=True,parents=True)
+Path(product["ABSOLUTE_PATH_TO_MEDALS_CSV_FILE"]).parent.mkdir(exist_ok=True, parents=True)
 Path(product["ABSOLUTE_PATH_TO_MEDALS_CSV_FILE"]).write_bytes(pickle.dumps(ABSOLUTE_PATH_TO_MEDALS_CSV_FILE))
-#print output
+# print output
 ABSOLUTE_PATH_TO_MEDALS_CSV_FILE
 
 # %%
 ABSOLUTE_PATH_TO_RESULTS_CSV_FILE = f"{ABSOLUTE_PATH_TO_DATA_FOLDER}/olympic_results.csv"
 # pickle it to use it later
-Path(product["ABSOLUTE_PATH_TO_RESULTS_CSV_FILE"]).parent.mkdir(exist_ok=True,parents=True)
+Path(product["ABSOLUTE_PATH_TO_RESULTS_CSV_FILE"]).parent.mkdir(exist_ok=True, parents=True)
 Path(product["ABSOLUTE_PATH_TO_RESULTS_CSV_FILE"]).write_bytes(pickle.dumps(ABSOLUTE_PATH_TO_RESULTS_CSV_FILE))
-#print output
+# print output
 ABSOLUTE_PATH_TO_RESULTS_CSV_FILE
 
 # %%
